cc804179d518c09bf6cae7b46e08f9eb5e72a529
陈新雄 <xxchen@haitaiinc.com>
2024-06-10 00:52:13 +0800

    first commit

---
+++ src/main/java/com/example/servicedispatcher/ServiceDispatcherApplication.java
@@ -0,0 +1,13 @@
+package com.example.servicedispatcher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceDispatcherApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceDispatcherApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/servicedispatcher/ServiceDispatcherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.servicedispatcher;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceDispatcherApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
